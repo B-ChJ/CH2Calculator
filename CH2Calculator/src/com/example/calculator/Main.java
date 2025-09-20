759bd889060281ef79340ada98a75f472d14dd8f
B-ChJ <bchaejoo@gmail.com>
2025-09-20 10:41:40 +0900

    Lv 1. 클래스 없이 기본적인 연산을 수행할 수 있는 계산기 만들기 ✔ 사칙연산 기호(+, -, ×, ÷)를 입력받기 ✔ 위에서 입력ㅂ다은 양의 정수 2개와 사칙연산 기호를 사용하여 연산을 진행한 후 결과값을 출력하기

--- CH2Calculator/src/com/example/calculator/Main.java
+++ CH2Calculator/src/com/example/calculator/Main.java
@@ -12,6 +12,33 @@ public class Main {
         System.out.println("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt();
 
+        System.out.println("사칙연산 기호를 입력하세요: ");
+        char operator = sc.next().charAt(0);
+
+        int result = 0;
+
+        switch(operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                //(예외 처리) 0으로 나누기
+                if(num2 == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
+                    break;}
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println(operator + "는 계산할 수 없는 연산자입니다.");
+                break;
+        }
+        System.out.println("result = " + result);
     }
 
 }
