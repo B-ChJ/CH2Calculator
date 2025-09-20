50a92b47a24bdbbc643269292445a1c1e1fc263c
B-ChJ <bchaejoo@gmail.com>
2025-09-20 10:10:20 +0900

    Lv 1. 클래스 없이 기본적인 연산을 수행할 수 있는 계산기 만들기 ✔ 양의 정수를 입력받기(0 포함)

---
+++ CH2Calculator/src/com/example/calculator/Main.java
@@ -0,0 +1,17 @@
+package com.example.calculator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("첫 번째 숫자를 입력하세요: ");
+        int num1 = sc.nextInt();
+
+        System.out.println("두 번째 숫자를 입력하세요: ");
+        int num2 = sc.nextInt();
+
+    }
+
+}
