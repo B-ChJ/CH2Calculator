e1f6acab8c6b2b450eeb72344bcd978f81bbab28
B-ChJ <bchaejoo@gmail.com>
2025-09-20 10:54:22 +0900

    Lv 1. 클래스 없이 기본적인 연산을 수행할 수 있는 계산기 만들기 ✔ 반복문을 사용하되, 반복의 종료를 알려주는 "exit" 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정하기

--- CH2Calculator/src/com/example/calculator/Main.java
+++ CH2Calculator/src/com/example/calculator/Main.java
@@ -4,41 +4,49 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("첫 번째 숫자를 입력하세요: ");
-        int num1 = sc.nextInt();
-
-        System.out.println("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
-
-        System.out.println("사칙연산 기호를 입력하세요: ");
-        char operator = sc.next().charAt(0);
-
         int result = 0;
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
 
-        switch(operator) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                //(예외 처리) 0으로 나누기
-                if(num2 == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
-                    break;}
-                result = num1 / num2;
-                break;
-            default:
-                System.out.println(operator + "는 계산할 수 없는 연산자입니다.");
-                break;
+        while(flag){
+
+            System.out.println("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
+
+            System.out.println("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
+
+            System.out.println("사칙연산 기호를 입력하세요: ");
+            char operator = sc.next().charAt(0);
+
+
+            switch(operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    //(예외 처리) 0으로 나누기
+                    if(num2 == 0) {
+                        System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
+                        break;}
+                    result = num1 / num2;
+                    break;
+                default:
+                    System.out.println(operator + "는 계산할 수 없는 연산자입니다.");
+                    break;
+            }
+            System.out.println("result = " + result);
+            System.out.println("더 계산하시겠습니까? Press Any key to Continue...\n(exit 입력 시 종료)");
+            if(sc.next().equals("exit")) {
+                flag = false;
+            }
         }
-        System.out.println("result = " + result);
     }
 
 }
